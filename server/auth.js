ff515ce1d43f842c3ae0b6156a10eb2793d82412
Falphir <73762798+Falphir@users.noreply.github.com>
2021-11-17 10:36:10 +0000

    17.11.2021

--- server/auth.js
+++ server/auth.js
@@ -1,31 +1,110 @@
-const bodyParser = require ('body-parser');
-const express = require ('express');
-const Users = require ('../data/users')
+const express = require('express');
+const Users = require('../data/users');
 
-function AuthRouter() {
-     let router = express();
-     
-     router.use(bodyParser.json({limit : '100mb'}));
-     router.use(bodyParser.urlencoded({ limit : '100mb', extended: true}));
+function AuthRouter(){
+    let router = express();
 
-     router.route('./register')
+    //camadas
+    router.use(express.json( {
+        limit: '100mb' }
+    ));
+
+    router.use(express.urlencoded( 
+       { limit: '100mb', extended: true }
+    ));
+    //fim camadas
+
+
+
+    //auth/register        
+    router.route('/register')
+        //POST - create user
         .post(function (req, res, next) {
+            console.log('---|create user|---');
             const body = req.body;
+
+            //console.log(body);
 
             Users.create(body)
                 .then((user) => Users.createToken(user))
+
                 .then((response) => {
+                    console.log('save');
                     res.status(200);
                     res.send(response);
                 })
+
                 .catch((err) => {
+                    console.log("error");
                     res.status(500);
                     res.send(err);
                     next();
                 });
         });
 
-    return router;
+
+
+    //auth/me
+    router.route('/me')
+        //GET - verify token
+        .get(function(req, res, next) {
+            console.log('---|verify token|---');
+            let token = req.headers['x-access-token'];
+
+            if(!token) {
+
+                return res.status(401).send({ auth: false, message: 'No token provided.' })
+            }
+
+            return Users.verifyToken(token)
+                .then((decoded) => {
+
+                    res.status(202).send({ auth: true, decoded });
+                })
+
+                .catch((err) => {
+                    console.log("error");
+                    res.status(500);
+                    res.send(err);
+                    next();
+                });
+        });
+
+
+
+    //auth/login
+    router.route('/login')
+        //POST - validar se o user existe na BD
+        .post(function (req, res, next) {
+            console.log('---|verifiy user if exists|---');
+            let name = req.body.name;
+            let password = req.body.password;
+
+            //console.log(name);
+            //console.log(password);
+
+            Users.findUser({ name, password })
+                .then((user) => Users.createToken(user))
+
+                .then((response) => {
+                    console.log('save');
+                    res.status(200);
+                    res.send(response);
+                })
+            
+                .catch((err) => {
+                    console.log("error");
+                    res.status(500);
+                    res.send(err);
+                    next();
+                });
+        });
+
+
+
+
+
+        return router;
 }
 
-module.exports = AuthRouter
+module.exports = AuthRouter;
